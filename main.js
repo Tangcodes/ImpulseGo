75fcc1209db75775e60d7ae7c98e57a63a3aac30
Tangcodes <tangjingxuan0612@gmail.com>
2020-08-14 23:19:04 +0000

    Set database

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ app.use(flash());
 app.locals.moment = require("moment");
 
 // MongoDB
-mongoose.connect("mongodb://localhost:27017/impulse_go", {
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/impulse_go"
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
